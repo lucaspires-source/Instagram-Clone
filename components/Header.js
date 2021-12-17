b906ae280d9c1d0aadc2b92301b07be74c4bc9be
lucaspires-source <pires.lucas94@gmail.com>
2021-12-17 11:19:41 -0300

    building left section of the header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
+      <div className="flex justify-between max-w-6xl">
+        <div className="relative hidden lg:inline-grid h-24 w-24">
+          <Image src="https://links.papareact.com/ocw" alt="logo" layout="fill" objectFit="contain" />
+        </div>
+      </div>
     </div>
   );
 }
