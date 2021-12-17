fd14a88d0c285fe161beed872614bea73da3368e
lucaspires-source <pires.lucas94@gmail.com>
2021-12-17 16:01:30 -0300

    implementing Sticky header

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ import { HomeIcon } from '@heroicons/react/solid';
 
 function Header() {
   return (
-    <div>
+    <div className="shadow-sm bg-white border-b sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
         {/* Left */}
         <div className="relative hidden lg:inline-grid w-24">
@@ -30,7 +30,10 @@ function Header() {
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navBtn" />
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
-          <PaperAirplaneIcon className="navBtn" />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+          </div>
           <PlusCircleIcon className="navBtn" />
           <UserGroupIcon className="navBtn" />
           <HeartIcon className="navBtn" />
