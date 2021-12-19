9c65e7741f8585df7011170c789a5611b3b74de7
lucaspires-source <pires.lucas94@gmail.com>
2021-12-19 06:39:10 -0300

    implementing Faker.js

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import faker from 'faker';
+import Story from './Story';
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+    setSuggestions(suggestions);
+  }, []);
   return (
     <div>
-      Stories
+      {suggestions.map((profile) => (
+        <Story key={profile.id} img={profile.avatar} username={profile.username} />
+      ))}
     </div>
   );
 }
---
+++ components/Story.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Story() {
+  return (
+    <div>
+      Story
+    </div>
+  );
+}
+
+export default Story;
