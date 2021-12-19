bfa90d152b046a01ab53b45bccca0347c10b2c5a
lucaspires-source <pires.lucas94@gmail.com>
2021-12-19 06:05:38 -0300

    initial sctructure of stories

--- components/Feed.js
+++ components/Feed.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import Stories from './Stories';
 
 function Feed() {
   return (
-    <div>
+    <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto ">
       {/*  Left Section */}
-      {/* Stories */}
-      {/* posts */}
+      <section className="col-span-2">
+        <Stories />
+        {/* posts */}
+      </section>
+
       {/*  Left Section */}
 
       {/*   Right Section */}
-      {/* Mini Profile */}
-      {/* Suggestions */}
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestions */}
+      </section>
       {/*   Right Section */}
-    </div>
+    </main>
   );
 }
 
