00c5faca056bc366ebb69b07c3b9a66af97b368e
lucaspires-source <pires.lucas94@gmail.com>
2021-12-22 10:30:52 -0300

    finalizing post component

--- components/Posts.js
+++ components/Posts.js
@@ -1,29 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  collection, onSnapshot, orderBy, query,
+} from '@firebase/firestore';
 import Post from './Post';
+import { db } from '../firebase';
 
-const posts = [
-  {
-    id: '123',
-    username: 'o_lucaspires',
-    userImg: 'https://avatars.githubusercontent.com/u/60105171?v=4',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Tiger_shark.jpg/1200px-Tiger_shark.jpg',
-    caption: 'Pai ta on',
-  },
-
-  {
-    id: '1234',
-    username: 'o_lucaspires',
-    userImg: 'https://avatars.githubusercontent.com/u/60105171?v=4',
-    img: 'https://universodospeixes.com.br/wp-content/uploads/2019/05/blacktip-reef-shark.jpg',
-    caption: 'E-Sharko',
-  },
-];
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), (snapshot) => {
+    setPosts(snapshot.docs);
+  }), [db]);
+
+  console.log(posts);
   return (
     <div>
       {posts.map((post) => (
         // eslint-disable-next-line max-len
-        <Post key={post.id} id={post.id} username={post.username} userImg={post.userImg} img={post.img} caption={post.caption} />
+        <Post key={post.id} id={post.id} username={post.data().username} userImg={post.data().profileImg} img={post.data().image} caption={post.data().caption} />
       ))}
     </div>
   );
