5ed96f88bf6fd5b98ce55182158ea5e7c33b6fee
lucaspires-source <pires.lucas94@gmail.com>
2021-12-22 12:50:54 -0300

    final touches

--- components/Post.js
+++ components/Post.js
@@ -73,6 +73,13 @@ function Post({
       }
 
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">
+            {likes.length}
+            {' '}
+            likes
+          </p>
+        )}
         <span className="font-bold mr-1">
           {username}
           {' '}
