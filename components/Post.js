eb37f74e3fd4d3a02d8c792571e05bb49f2061c1
lucaspires-source <pires.lucas94@gmail.com>
2021-12-22 11:13:51 -0300

    implementing commment functionality

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BookmarkIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, PaperAirplaneIcon, EmojiHappyIcon,
 } from '@heroicons/react/outline';
 import PropTypes from 'prop-types';
 import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
+import { useSession } from 'next-auth/react';
+import { addDoc, collection, serverTimestamp } from '@firebase/firestore';
+import { db } from '../firebase';
 
 function Post({
   id, username, userImg, caption, img,
 }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+
+    const commentToSend = comment;
+    setComment('');
+    await addDoc(collection(db, 'posts', id, 'comments'), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
   return (
     <div className="bg-white my-7 border rounded-sm">
       <div className="flex items-center p-5">
@@ -16,14 +35,21 @@ function Post({
         <DotsHorizontalIcon className="h-5" />
       </div>
       <img src={img} alt="post" className="object-cover w-full" />
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <HeartIcon className="postBtn" />
-          <ChatIcon className="postBtn" />
-          <PaperAirplaneIcon className="postBtn" />
+      {
+        session
+
+        && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="postBtn" />
+            <ChatIcon className="postBtn" />
+            <PaperAirplaneIcon className="postBtn" />
+          </div>
+          <BookmarkIcon className="postBtn" />
         </div>
-        <BookmarkIcon className="postBtn" />
-      </div>
+        )
+      }
+
       <p className="p-5 truncate">
         <span className="font-bold mr-1">
           {username}
@@ -31,12 +57,18 @@ function Post({
         </span>
         {caption}
       </p>
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7" />
-        <input type="text" className="border-none flex-1 focus:ring-0" placeholder="Add a comment ..." />
-        {/* eslint-disable-next-line react/button-has-type */}
-        <button className="font-semibold text-blue-400"> Post </button>
-      </form>
+      {
+        session
+
+        && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7" />
+          <input type="text" className="border-none flex-1 focus:ring-0" placeholder="Add a comment ..." value={comment} onChange={(e) => setComment(e.target.value)} />
+          <button className="font-semibold text-blue-400" type="submit" disabled={!comment.trim()} onClick={sendComment}> Post </button>
+        </form>
+        )
+      }
+
     </div>
   );
 }
